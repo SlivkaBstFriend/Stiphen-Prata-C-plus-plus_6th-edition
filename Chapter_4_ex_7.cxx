9292d9b6edbdc71e9795405cc3d2fe6d89e31605
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:50:25 +0300

    Update Chapter_4_ex_7.cxx

--- Chapter_4_ex_7.cxx
+++ Chapter_4_ex_7.cxx
@@ -1,6 +1,15 @@
+/*William Wingate runs a pizza-analysis service. For each pizza, he needs to record
+the following information:
+n The name of the pizza company, which can consist of more than one word
+n The diameter of the pizza
+n The weight of the pizza
+Devise a structure that can hold this information and write a program that uses a
+structure variable of that type.The program should ask the user to enter each of the
+preceding items of information,and then the program should display that information. Use cin (or its methods) and cout*/
+
 #include <iostream>
 #include <string>
-#include <locale>
+#include <locale.h>
 
 using namespace std;
 
