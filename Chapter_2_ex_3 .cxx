ada61f598934d86048d84677eaa8fd56579e7ec0
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:34:26 +0300

    Update Chapter_2_ex_3 .cxx

--- Chapter_2_ex_3 .cxx
+++ Chapter_2_ex_3 .cxx
@@ -3,7 +3,9 @@ one) and produces the following output:
 Three blind mice
 Three blind mice
 See how they run
-See how they run*/
+See how they run
+One function, called two times, should produce the first two lines,and the remain
+ing function,also called twice, should produce the remaining output*/
 
 #include <iostream> 
 #include <locale.h>
