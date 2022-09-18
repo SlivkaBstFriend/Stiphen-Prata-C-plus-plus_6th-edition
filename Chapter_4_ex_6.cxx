7ce004136a143ecdbc57d2eb0837e3938c91bc47
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:49:50 +0300

    Update Chapter_4_ex_6.cxx

--- Chapter_4_ex_6.cxx
+++ Chapter_4_ex_6.cxx
@@ -1,3 +1,7 @@
+/*The CandyBar structure contains three members,as described in Programming
+Exercise 5.Write a program that creates an array of three CandyBar structures, initializes
+them to values of your choice,and then displays the contents of each structure*/
+
 #include <iostream>
 #include <string>
 
