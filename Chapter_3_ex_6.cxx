53e665fef1ebb8706c83bf7ffb248b5de972ffd7
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:42:26 +0300

    Update Chapter_3_ex_6.cxx

--- Chapter_3_ex_6.cxx
+++ Chapter_3_ex_6.cxx
@@ -1,3 +1,8 @@
+/*Write a program that asks how many miles you have driven and how many gallons
+of gasoline you have used and then reports the miles per gallon your car has gotten.
+Or, if you prefer, the program can request distance in kilometers and petrol in liters
+and then report the result European style, in liters per 100 kilometers*/
+
 #include <iostream>
 #include <locale.h> 
 
