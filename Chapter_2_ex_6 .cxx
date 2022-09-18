cc3344f2eef6abefcd194ec5bbc743f1eb6fc406
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:37:10 +0300

    Update Chapter_2_ex_6 .cxx

--- Chapter_2_ex_6 .cxx
+++ Chapter_2_ex_6 .cxx
@@ -3,7 +3,13 @@ in light years as an argument and then returns the distance in astronomical unit
 The program should request the light year value as input from the user and display
 the result,as shown in the following code:
 Enter the number of light years: 4.2
-4.2 light years = 265608 astronomical units.*/
+4.2 light years = 265608 astronomical units.
+
+An astronomical unit is the average distance from the earth to the sun (about
+150,000,000 km or 93,000,000 miles),and a light year is the distance light travels in
+a year (about 10 trillion kilometers or 6 trillion miles). (The nearest star after the
+sun is about 4.2 light years away.) Use type double (as in Listing 2.4) and this conversion factor:
+1 light year = 63,240 astronomical units*/
 
 #include <iostream>
 #include <locale.h>
