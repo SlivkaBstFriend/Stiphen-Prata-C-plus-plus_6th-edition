4f8bcb2789c65eb445c546efcea1646198482501
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:52:17 +0300

    Update Chapter_5_ex_1.cxx

--- Chapter_5_ex_1.cxx
+++ Chapter_5_ex_1.cxx
@@ -1,3 +1,9 @@
+/*Write a program that requests the user to enter two integers.The program should
+then calculate and report the sum of all the integers between and including the two
+integers.At this point,assume that the smaller integer is entered first. For example, if
+the user enters 2 and 9, the program should report that the sum of all the integers
+from 2 through 9 is 44.*/
+
 #include <iostream>
 #include <locale.h>
 
