8b04afcf4a8ad1a57da4a5215ba0c0cec4c92692
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:38:02 +0300

    Update Chapter_2_ex_7 .cxx

--- Chapter_2_ex_7 .cxx
+++ Chapter_2_ex_7 .cxx
@@ -1,3 +1,9 @@
+/*Write a program that asks the user to enter an hour value and a minute value.The
+main() function should then pass these two values to a type void function that displays the two values in the format shown in the following sample run:
+Enter the number of hours: 9
+Enter the number of minutes: 28
+Time: 9:28*/
+
 #include <iostream>
 #include <locale.h>
 
