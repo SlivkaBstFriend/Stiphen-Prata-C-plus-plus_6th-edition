0d9c4a89fded45103ae66668d6fb6292902dbf82
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 21:06:15 +0300

    Update Chapter_5_ex_9.cxx

--- Chapter_5_ex_9.cxx
+++ Chapter_5_ex_9.cxx
@@ -1,3 +1,7 @@
+/*Write a program that matches the description of the program in Programming
+Exercise 8, but use a string class object instead of an array. Include the string
+header file and use a relational operator to make the comparison test.*/
+
 #include <iostream>
 #include <string>
 #include <locale>
