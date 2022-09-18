0f243b93ee4627277e02a6e4566c9820f5234c66
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:40:01 +0300

    Update Chapter_3_ex_2.cxx

--- Chapter_3_ex_2.cxx
+++ Chapter_3_ex_2.cxx
@@ -1,3 +1,12 @@
+/*Write a short program that asks for your height in feet and inches and your weight
+in pounds. (Use three variables to store the information.) Have the program report
+your body mass index (BMI).To calculate the BMI, first convert your height in feet
+and inches to your height in inches (1 foot = 12 inches).Then convert your height
+in inches to your height in meters by multiplying by 0.0254.Then convert your
+weight in pounds into your mass in kilograms by dividing by 2.2. Finally, compute
+your BMI by dividing your mass in kilograms by the square of your height in
+meters. Use symbolic constants to represent the various conversion factors.*/
+
 #include <iostream>
 #include <locale.h>
 
