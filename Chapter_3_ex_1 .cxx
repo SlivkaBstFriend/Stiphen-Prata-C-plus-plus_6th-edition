910072128eaf8c67b10fe30220acba1e9b257370
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:39:18 +0300

    Update Chapter_3_ex_1 .cxx

--- Chapter_3_ex_1 .cxx
+++ Chapter_3_ex_1 .cxx
@@ -1,6 +1,10 @@
+/*Write a short program that asks for your height in integer inches and then converts
+your height to feet and inches. Have the program use the underscore character to
+indicate where to type the response.Also use a const symbolic constant to represent the conversion factor.*/
+
 #include <iostream>
 #include <locale.h>
-#include <cmath>
+
 using namespace std;
 
 int main()
