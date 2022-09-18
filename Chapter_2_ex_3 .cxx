e08284f97037a6e385c9a619a027a2e3725cf6d5
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:33:22 +0300

    Update Chapter_2_ex_3 .cxx

--- Chapter_2_ex_3 .cxx
+++ Chapter_2_ex_3 .cxx
@@ -1,3 +1,10 @@
+/*Write a C++ program that uses three user-defined functions (counting main() as
+one) and produces the following output:
+Three blind mice
+Three blind mice
+See how they run
+See how they run*/
+
 #include <iostream> 
 #include <locale.h>
 
