c0995d3395375d3175676e601b0fcad01617d6de
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:40:30 +0300

    Update Chapter_3_ex_3.cxx

--- Chapter_3_ex_3.cxx
+++ Chapter_3_ex_3.cxx
@@ -1,3 +1,12 @@
+/*Write a program that asks the user to enter a latitude in degrees, minutes,and seconds and that then displays the latitude in decimal format.There are 60 seconds of
+arc to a minute and 60 minutes of arc to a degree; represent these values with symbolic constants.You should use a separate variable for each input value.A sample
+run should look like this:
+Enter a latitude in degrees, minutes, and seconds:
+First, enter the degrees: 37
+Next, enter the minutes of arc: 51
+Finally, enter the seconds of arc: 19
+37 degrees, 51 minutes, 19 seconds = 37.8553 degrees*/
+
 #include <iostream>
 #include <locale.h>
 
