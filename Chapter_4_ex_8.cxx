940feaf4e53404a8aae0c6c7f31b5a7daddfbb44
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:50:55 +0300

    Update Chapter_4_ex_8.cxx

--- Chapter_4_ex_8.cxx
+++ Chapter_4_ex_8.cxx
@@ -1,6 +1,10 @@
+/*Do Programming Exercise 7 but use new to allocate a structure instead of declaring
+a structure variable.Also have the program request the pizza diameter before it
+requests the pizza company name.*/
+
 #include <iostream>
 #include <string>
-#include <locale>
+#include <locale.h>
 
 using namespace std;
 
