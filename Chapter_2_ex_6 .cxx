5d3bc690ec232a76521bd4b6509808a098d557dd
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:35:26 +0300

    Update Chapter_2_ex_6 .cxx

--- Chapter_2_ex_6 .cxx
+++ Chapter_2_ex_6 .cxx
@@ -1,3 +1,10 @@
+/*Write a program that has main() call a user-defined function that takes a distance
+in light years as an argument and then returns the distance in astronomical units.
+The program should request the light year value as input from the user and display
+the result,as shown in the following code:
+Enter the number of light years: 4.2
+4.2 light years = 265608 astronomical units.*/
+
 #include <iostream>
 #include <locale.h>
 
