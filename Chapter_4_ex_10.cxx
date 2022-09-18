1740bd2cf5be44ce8e448a6b88aa40f0d7b13b9c
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 20:51:45 +0300

    Update Chapter_4_ex_10.cxx

--- Chapter_4_ex_10.cxx
+++ Chapter_4_ex_10.cxx
@@ -1,5 +1,9 @@
+/*Write a program that requests the user to enter three times for the 40-yd dash (or
+40-meter, if you prefer) and then displays the times and the average. Use an array
+object to hold the data. (Use a built-in array if array is not available.)*/
+
 #include <iostream>
-#include <locale>
+#include <locale.h>
 #include <array>
 
 using namespace std;
