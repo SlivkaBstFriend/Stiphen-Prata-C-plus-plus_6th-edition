983a061e8061b8e41810f5244bf9ae87721bc7bf
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-18 21:03:03 +0300

    Update Chapter_5_ex_5.cxx

--- Chapter_5_ex_5.cxx
+++ Chapter_5_ex_5.cxx
@@ -1,3 +1,10 @@
+/*You sell the book C++ for Fools.Write a program that has you enter a year’s worth
+of monthly sales (in terms of number of books, not of money).The program should
+use a loop to prompt you by month, using an array of char * (or an array of
+string objects, if you prefer) initialized to the month strings and storing the input
+data in an array of int.Then, the program should find the sum of the array
+contents and report the total sales for the yea*/
+
 #include <iostream>
 #include <locale.h>
 
