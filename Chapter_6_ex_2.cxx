4fa2db5294aeb0752e21d510a1100be8e3ece50f
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-25 18:32:27 +0300

    Update Chapter_6_ex_2.cxx

--- Chapter_6_ex_2.cxx
+++ Chapter_6_ex_2.cxx
@@ -1,3 +1,10 @@
+/*
+Write a program that reads up to 10 donation values into an array of double. (Or, if
+you prefer, use an array template object.) The program should terminate input on
+non-numeric input. It should report the average of the numbers and also report
+how many numbers in the array are larger than the average.
+*/
+
 #include <iostream>
 #include <locale.h>
 #include <array>
