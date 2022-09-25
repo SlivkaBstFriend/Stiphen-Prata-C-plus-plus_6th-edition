5536e7497b65f44a9d0cf973c7340d98628656b9
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-25 18:31:32 +0300

    Update Chapter_6_ex_3.cpp

--- Chapter_6_ex_3.cpp
+++ Chapter_6_ex_3.cpp
@@ -1,4 +1,20 @@
-﻿#include <iostream>
+/*
+Write a precursor to a menu-driven program.The program should display a menu
+offering four choices, each labeled with a letter. If the user responds with a letter
+other than one of the four valid choices, the program should prompt the user to
+enter a valid response until the user complies.Then the program should use a
+switch to select a simple action based on the user’s selection.A program run could
+look something like this:
+Please enter one of the following choices:
+c) carnivore p) pianist
+t) tree g) game
+f
+Please enter a c, p, t, or g: q
+Please enter a c, p, t, or g: t
+A maple is a tree.
+*/
+
+#include <iostream>
 
 using namespace std;
 void showmenu();
