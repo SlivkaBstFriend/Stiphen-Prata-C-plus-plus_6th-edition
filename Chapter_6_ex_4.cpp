3b89ca3d439688ede541b049ee592a9cbcd4bb92
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-27 22:03:20 +0300

    Update Chapter_6_ex_4.cpp

--- Chapter_6_ex_4.cpp
+++ Chapter_6_ex_4.cpp
@@ -1,9 +1,56 @@
-﻿#include <iostream>
+/*
+When you join the Benevolent Order of Programmers, you can be known at BOP
+meetings by your real name, your job title, or your secret BOP name.Write a program 
+that can list members by real name, by job title, by secret name, or by a member’s preference.
+Base the program on the following structure:
+
+            // Benevolent Order of Programmers name structure
+            struct bop {
+            char fullname[strsize]; // real name
+            char title[strsize]; // job title
+            char bopname[strsize]; // secret BOP name
+            int preference; // 0 = fullname, 1 = title, 2 = bopname
+            };
+
+In the program, create a small array of such structures and initialize it to suitable
+values. Have the program run a loop that lets the user select from different alternatives:
+            a. display by name b. display by title
+            c. display by bopname d. display by preference
+            q. quit
+
+Note that “display by preference” does not mean display the preference member; it
+means display the member corresponding to the preference number. For instance, if
+preference is 1, choice d would display the programmer’s job title.A sample run
+may look something like the following:
+
+            Benevolent Order of Programmers Report
+            a. display by name b. display by title
+            c. display by bopname d. display by preference
+            q. quit
+            Enter your choice: a
+            Wimp Macho
+            Raki Rhodes
+            Celia Laiter
+            Hoppy Hipman
+            Pat Hand
+            Next choice: d
+            Wimp Macho
+            Junior Programmer
+            MIPS
+            Analyst Trainee
+            LOOPY
+            Next choice: q
+            Bye!
+*/
+
+
+
+#include <iostream>
 #include <locale.h>
 
 void showmenu();
-using namespace std;
 
+using namespace std;
 const int strsize = 80;
 struct bop
 {
