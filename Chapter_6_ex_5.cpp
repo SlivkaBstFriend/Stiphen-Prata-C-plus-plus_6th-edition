59efc3ad33ec2b92240df716d65b0d7bfccc3590
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-27 23:06:20 +0300

    Update Chapter_6_ex_5.cpp

--- Chapter_6_ex_5.cpp
+++ Chapter_6_ex_5.cpp
@@ -1,7 +1,17 @@
-﻿#include <iostream>
-#include <locale.h>
-#include <cctype>
+/*
+The Kingdom of Neutronia, where the unit of currency is the tvarp, has the following income tax code:
+        First 5,000 tvarps: 0% tax
+        Next 10,000 tvarps: 10% tax
+        Next 20,000 tvarps: 15% tax
+        Tvarps after 35,000: 20% tax
+For example, someone earning 38,000 tvarps would owe 5,000 × 0.00 + 10,000 ×
+0.10 + 20,000 × 0.15 + 3,000 × 0.20, or 4,600 tvarps.Write a program that uses a
+loop to solicit incomes and to report tax owed.The loop should terminate when
+the user enters a negative number or non-numeric input.
+*/
 
+#include <iostream>
+#include <locale.h>
 
 using namespace std;
 
