8c33b5e286f2349adf8188c044e85fcd05fdc17b
Jessica <47040622+SlivkaBstFriend@users.noreply.github.com>
2022-09-29 00:33:08 +0300

    Update Chapter_6_ex_6.cpp

--- Chapter_6_ex_6.cpp
+++ Chapter_6_ex_6.cpp
@@ -1,4 +1,19 @@
-﻿#include <iostream>
+/*
+Put together a program that keeps track of monetary contributions to the Society
+for the Preservation of Rightful Influence. It should ask the user to enter the number 
+of contributors and then solicit the user to enter the name and contribution of
+each contributor.The information should be stored in a dynamically allocated array
+of structures. Each structure should have two members:a character array (or else a
+string object) to store the name and a double member to hold the amount of the
+contribution.After reading all the data, the program should display the names and
+amounts donated for all donors who contributed $10,000 or more.This list should
+be headed by the label Grand Patrons.After that, the program should list the
+remaining donors.That list should be headed Patrons. If there are no donors in one
+of the categories, the program should print the word “none.”Aside from displaying
+two categories, the program need do no sorting
+*/
+
+#include <iostream>
 #include <locale.h>
 #include <string>
 
